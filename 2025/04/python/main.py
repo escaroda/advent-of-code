2a16ea3b71063f259b0b29b3f35102429cdc6e9d
Dima Escaroda <escaroda@gmail.com>
2025-12-04 12:31:42 +0300

    2025 day 4 python

---
+++ 2025/04/python/main.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+from typing import List
+
+
+ROLL_CHAR = "@"
+EMPTY_CHAR = "."
+ROLLS_MAX = 3
+
+dirs = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
+
+
+def read_file(path: str) -> str:
+    with open(path, encoding="utf-8") as f:
+        return f.read()
+
+
+def remove_rolls(grid: List[List[str]]) -> tuple[int, List[List[str]]]:
+    rolls = 0
+    grid_updated = [x[:] for x in grid]
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] != ROLL_CHAR:
+                continue
+
+            count = 0
+            for di, dj in dirs:
+                if (
+                    i + di < 0
+                    or i + di >= len(grid)
+                    or j + dj < 0
+                    or j + dj >= len(grid[i])
+                ):
+                    continue
+
+                if grid[i + di][j + dj] == ROLL_CHAR:
+                    count += 1
+
+            if count <= ROLLS_MAX:
+                grid_updated[i][j] = EMPTY_CHAR
+                rolls += 1
+
+    return rolls, grid_updated
+
+
+def part1(data: str) -> None:
+    grid = list(map(list, data.splitlines()))
+    count, _ = remove_rolls(grid)
+
+    print("Part 1:", count)
+
+
+def part2(data: str) -> None:
+    grid = list(map(list, data.splitlines()))
+    res = 0
+    while True:
+        count, grid = remove_rolls(grid)
+        if count == 0:
+            break
+
+        res += count
+
+    print("Part 2:", res)
+
+
+def main():
+    print("--- Day 4: Printing Department ---")
+    print("https://adventofcode.com/2025/day/4\n")
+
+    data = read_file(str(Path(__file__).parent.parent / "input.txt"))
+    part1(data)
+    part2(data)
+
+
+if __name__ == "__main__":
+    main()
